eb41327be1a8992a08c3cfc8a331f3ecd339e285
James Dang <jdang@mymail.mines.edu>
2014-09-19 16:19:53 -0600

    Pictures change

--- Unit1/Catdoku.js
+++ Unit1/Catdoku.js
@@ -1,27 +1,61 @@
 window.onload=function() {
 	var catTable;
-		catTable = "<table border = '1' style=\"margin-right:auto; margin-left:auto; width:50%;\">"
-		for (i=0; i<3; i++) {
-			catTable = catTable + "<tr>";
+		catTable = "<table>"
+		for (i=1; i<=3; i++) {
+			catTable = catTable + "<tr><td>";
 			for (j=0; j<3; j++) {
-				catTable = catTable + "<tr>";
+				catTable = catTable + "<td><table><tr>";
 				for (k=0; k<3; k++) {
-					catTable  = catTable + "<td id = \"cat"+i+j+k+"\" align=\"center\"><img src=\"one.jpg\" height=\"30%\" ></td>";
+					catTable  = catTable + "<td><img id=\"cat"+i+j+k+"\" onclick=\"catClick("+i+j+k+")\" src=\"one.jpg\"></td>";
 				}
-				for (l=0; l<3; l++) {
-					catTable  = catTable + "<td id = \"cat"+i+j+l+"\"align=\"center\"><img src=\"two.jpg\" height=\"30%\"></td>";
+				catTable = catTable + "</tr><tr>";
+				for (l=3; l<6; l++) {
+					catTable  = catTable + "<td><img id=\"cat"+i+j+l+"\" onclick=\"catClick("+i+j+l+")\" src=\"two.jpg\"></td>";
 				}
-				for (l=0; l<3; l++) {
-					catTable  = catTable + "<td id = \"cat"+i+j+l+"\"align=\"center\"><img src=\"seven.jpg\" height=\"30%\"></td>";
+				catTable = catTable + "</tr><tr>";
+				for (m=6; m<9; m++) {
+					catTable  = catTable + "<td><img id=\"cat"+i+j+m+"\" onclick=\"catClick("+i+j+m+")\" src=\"seven.jpg\"></td>";
 				}
-				catTable = catTable + "</tr>";
+				catTable = catTable + "</tr></table></td>";
 			}
-			catTable = catTable + "</tr>";
+			catTable = catTable + "</td></tr>";
 		}
 		catTable = catTable + "</table>";
-		document.write(catTable);
+		document.getElementById("cats").innerHTML = catTable;
 }
-
 function initilize() {
 }
 
+function catClick(catPos) {
+	var img = document.getElementById("cat"+catPos);
+	var str = img.src.substring(img.src.lastIndexOf("/")+1,img.src.length);
+	switch (str) {
+		case "one.jpg":
+			img.src="two.jpg";
+			break;
+		case "two.jpg":
+			img.src="three.jpg";
+			break;
+		case "three.jpg":
+			img.src="four.jpg";
+			break;
+		case "four.jpg":
+			img.src="five.jpg";
+			break;
+		case "five.jpg":
+			img.src="six.jpg";
+			break;
+		case "six.jpg":
+			img.src="seven.jpg";
+			break;
+		case "seven.jpg":
+			img.src="eight.jpg";
+			break;
+		case "eight.jpg":
+			img.src="nine.jpg";
+			break;
+		case "nine.jpg":
+			img.src="one.jpg";
+			break;
+	}
+}
